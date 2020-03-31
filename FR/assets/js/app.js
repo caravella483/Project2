e33c3a52435556f93500931c2ae9d0a965176024
Franchesca <frivas3817@gmail.com>
2020-03-31 18:26:06 -0400

    updated table

---
+++ FR/assets/js/app.js
@@ -0,0 +1,45 @@
+// Define SVG area dimensions
+var svgWidth = 960;
+var svgHeight = 660;
+
+// Define the chart's margins as an object
+var chartMargin = {
+    top: 30,
+    right: 30,
+    bottom: 30,
+    left: 30
+};
+
+// Define dimensions of the chart area
+var chartWidth = svgWidth - chartMargin.left - chartMargin.right;
+var chartHeight = svgHeight - chartMargin.top - chartMargin.bottom;
+
+// Select body, append SVG area to it, and set the dimensions
+var svg = d3.select("body")
+    .append("svg")
+    .attr("height", svgHeight)
+    .attr("width", svgWidth);
+
+// Append a group to the SVG area and shift ('translate') it to the right and to the bottom
+var chartGroup = svg.append("g")
+    .attr("transform", `translate(${chartMargin.left}, ${chartMargin.top})`);
+
+// Load data from hours-of-tv-watched.csv
+d3.csv('./assets/data/mvdeaths.csv').then(function (data) {
+    console.log(data);
+    var tbody = d3.select('tbody');
+
+    tbody.html('');
+
+    data.forEach(dataRow => {
+        var row = tbody.append('tr');
+
+        Object.values(dataRow).forEach(val => {
+            var cell = row.append('td');
+
+            cell.text(val);
+        })
+    })
+
+})
+
---
+++ FR/assets/js/config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoiZnJpdmFzMzgxNyIsImEiOiJjazdtaHNtN3QwM3BoM2txODVtNnYybGVmIn0.Lu2p85aKhLUenI9QfamaRA"
