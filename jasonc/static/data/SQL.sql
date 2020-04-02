8c28729741e70f44654b8247a78832f04c39a737
Jason Caravella <jcaravella@access.com>
2020-04-02 13:18:02 -0400

    postgres

---
+++ jasonc/static/data/SQL.sql
@@ -0,0 +1,14 @@
+--Happiness
+create table 
+legalstatus_level (
+ID SERIAL,
+legalWeedStatus varchar(30),
+Year int,
+Mv_Deaths  int,
+Total_Deaths  int,
+Total_OD_Deaths int,
+OverdosePercent int,
+MVPercent int)
+;
+      
+	
